cf40a511111003efa2a5df6997fdab9d87744f95
Hans <hansmaesalu@gmail.com>
2011-05-28 01:45:04 +0300

    Circle drawing using canvas.

--- static/main.js
+++ static/main.js
@@ -2,11 +2,30 @@
  * Main JavaScript file for Spatially Invariant Vector Quantization
  */
 var main = {
+	
+	/*
+	 * UI elements
+	 */
+	canvasOriginal: null,
+	divOriginal: null,
+	divResult: null,
+	formOptions: null,
+	inputRadius: null,
+	inputX: null,
+	inputY: null,
+	inputImageName: null,
+	
+	/*
+	 * Original image
+	 */
+	imageOriginal: null,
 
 	/*
 	 * Image uploaded
 	 */
 	processUpload: function() {
+		main.imageOriginal = null;
+
 		// process response
 		try {
 			var response = $(this).contents().find("body").html();
@@ -16,44 +35,75 @@ var main = {
 			return;
 		}
 
-		// show error message, when there is error
-		var errorDiv = $("#uploadForm").prev(".error").hide();
+		// show error message
 		if (response.Error) {
-			if (errorDiv.size() == 0) {
-				errorDiv = $(document.createElement("div")).addClass("error")
-								.insertBefore($("#uploadForm"));
-			}
-			errorDiv.show().html(response.Message);
+			main.showError(response.Message);
 			return;
 		}
+		main.hideError();
 
-		$("#original").show().html('Loading...').css({width: "auto", height: "auto"});
-		$("#result").show().html('No results yet.').css({width: "auto", height: "auto"});
-		$("#optionsForm").show().find("input:text").val("");
-		$("#imageName").val(response.Image);
+		// show canvas
+		main.divOriginal.show();
+		main.canvasOriginal.width = 400;
+		main.canvasOriginal.height = 50;
 
+		// loading message
+		var ctx = main.canvasOriginal.getContext("2d");
+		ctx.fillStyle = "black";
+	    ctx.font="10pt Arial";
+		ctx.textBaseline = "middle";
+		ctx.clearRect(0,0,400,100);
+		ctx.fillText("Loading image...", 10, 25);
+
+		// show options form
+		main.formOptions.show();
+
+		// load image
 		var imageUrl = "/img/"+ response.Image;
-		var loadImage = new Image();
-		loadImage.onload = function() {
-			var width = loadImage.width;
-			var height = loadImage.height;
-
+		main.imageOriginal = new Image();
+		main.imageOriginal.onload = function() {
+			var width = this.width;
+			var height = this.height;
+			
 			console.log("Image ("+ width +" x "+ height +"; "+ imageUrl +")");
+			
+			// resize canvas
+			main.canvasOriginal.width = width;
+			main.canvasOriginal.height = height;
 
-			$("#original").show().html('<img src="'+ imageUrl +'" alt="" />')
-				.css({width: width, height: height});
+			// insert image to canvas
+			ctx.drawImage(this, 0, 0);
 		};
-		loadImage.src = imageUrl;
+		main.imageOriginal.src = imageUrl;
+		
+		main.inputImageName.val(response.Image);
+		main.divResult.show()
+			.html('No results yet.')
+	},
+
+	/*
+	 * Show error message
+	 */
+	showError: function(message) {
+		var errorDiv = $("#uploadForm").prev(".error");
+		if (errorDiv.size() == 0) {
+			errorDiv = $(document.createElement("div")).addClass("error")
+							.insertBefore($("#uploadForm"));
+		}
+		errorDiv.show().html(message);
+	},
+	hideError: function() {
+		$("#uploadForm").prev(".error").hide()
 	},
 	
 	/*
 	 * Get resulting image
 	 */
 	calcResult: function(form) {
-		$("#result").show().html('Loading...').css({width: "auto", height: "auto"});
+		main.divResult.show().html('Loading...').css({width: "auto", height: "auto"});
 		
-		var imageUrl = "/result/"+ $("#imageName").val() +"?x="+ parseInt($("#vectorX").val())
-							+"&y="+ parseInt($("#vectorY").val()) +"&r="+ parseInt($("#vectorRadius").val());
+		var imageUrl = "/result/"+ main.inputImageName.val() +"?x="+ parseInt(main.inputX.val())
+							+"&y="+ parseInt(main.inputY.val()) +"&r="+ parseInt(main.inputRadius.val());
 		var loadImage = new Image();
 		loadImage.onload = function() {
 			var width = loadImage.width;
@@ -61,7 +111,7 @@ var main = {
 
 			console.log("Result image ("+ width +" x "+ height +"; "+ imageUrl +")");
 
-			$("#result").show().html('<img src="'+ imageUrl +'" alt="" />')
+			main.divResult.show().html('<img src="'+ imageUrl +'" alt="" />')
 				.css({width: width, height: height});
 		};
 		loadImage.src = imageUrl;
@@ -71,17 +121,72 @@ var main = {
 	 * Get coordinates from click on image 
 	 */
 	coordinates: function(e) {
+		if (main.imageOriginal == null) {
+			return;
+		}
+
 		var offset = $(this).offset();
 		var vectorX = e.pageX - offset.left;
 		var vectorY = e.pageY - offset.top;
 		console.log("Click at "+ vectorX +" "+ vectorY +"");
-		$("#vectorX").val(vectorX);
-		$("#vectorY").val(vectorY);
+
+		main.inputX.val(vectorX);
+		main.inputY.val(vectorY);
+		
+		main.drawVector();
+	},
+
+	/*
+	 * Draw vector on original image
+	 */
+	drawVector: function() {
+		if (main.imageOriginal == null) {
+			return;
+		}
+
+		// input
+		var x = parseInt(main.inputX.val());
+		var y = parseInt(main.inputY.val());
+		var r = parseInt(main.inputRadius.val());
+		if (x < 0 || y < 0 || r <= 0) {
+			return;
+		}
+
+		// draw vector
+		var ctx = main.canvasOriginal.getContext("2d");
+		ctx.clearRect(0, 0, main.imageOriginal.width, main.imageOriginal.height);
+		ctx.drawImage(main.imageOriginal, 0, 0);
+
+		ctx.beginPath();
+		ctx.arc(x, y, r, 0, Math.PI*2, false); 
+		ctx.closePath();
+		ctx.stroke();
+	},
+	
+	/*
+	 * Vector input fields change event handler
+	 */
+	vectorChanged: function(e) {
+		if (this.value == "") {
+			return;
+		}
+		main.drawVector();
 	},
 
 	init: function() {
+		// UI elements
+		main.divOriginal = $("#original");
+		main.divResult = $("#result");
+		main.canvasOriginal = document.createElement("canvas");
+		main.divOriginal.get(0).appendChild(main.canvasOriginal);
+		main.formOptions = $("#optionsForm");
+		main.inputRadius = $("#vectorRadius").keyup(main.vectorChanged);
+		main.inputX = $("#vectorX").keyup(main.vectorChanged);
+		main.inputY = $("#vectorY").keyup(main.vectorChanged);
+		main.inputImageName = $("#imageName");
+
 		$("#uploadResponse").load(main.processUpload);
-		$("#original").delegate("img", "click", main.coordinates);
+		$("#original").delegate("canvas", "click", main.coordinates);
 		$("#optionsForm").submit(function(e) {
 			e.preventDefault();
 			main.calcResult($(this));
